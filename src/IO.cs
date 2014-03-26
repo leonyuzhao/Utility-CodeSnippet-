713e54c830bedfdefe4efa5b0be84fbb08e4e40e
Leon <sephirothyzoe@gmail.com>
2014-03-26 16:59:24 +0000

    Shared utility functions for IO operations and Web operations

---
+++ src/IO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+/// <summary>
+/// Wrapped Quick IO Utilities
+/// <summary>
+namespace Utility.IO
+{
+  public class File
+  {
+    public static string ReadContent(string filePath)
+    {
+      string content = string.Empty;
+      if (System.IO.File.Exists(filePath))
+      {
+        try
+        {
+          using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
+          {
+            content = reader.ReadToEnd();
+            reader.Close();
+          }
+        }
+        catch (System.IO.IOException ex)
+        {
+          throw ex;
+        }
+      }
+      return content;
+    }
+
+    public static bool WriteContent(string filePath, string appendedContent, bool append)
+    {
+      bool writeSuccess = false;
+      if (!System.IO.File.Exists(filePath))
+      {
+        try
+        {
+          using (System.IO.FileStream fs = System.IO.File.Create(filePath)) { }
+        }
+        catch (System.IO.IOException ex)
+        {
+          throw ex;
+        }
+      }
+      try
+      {
+        /// <note>
+        /// TODO 
+        /// Add synclock to prevent multi-thread conflict
+        /// <note>
+        using (System.IO.StreamWriter writer = new System.IO.StreamWriter(filePath, append))
+        {
+          writer.WriteLine(appendedContent);
+          writer.Close();
+          writeSuccess = true;
+        }
+      }
+      catch (System.IO.IOException ex)
+      {
+        throw ex;
+      }
+      return writeSuccess;
+    }
+
+    public static bool DeleteFile(string filePath)
+    {
+      bool deleteSuccess = false;
+      if (System.IO.File.Exists(filePath))
+      {
+        try
+        {
+          System.IO.File.Delete(filePath);
+          deleteSuccess = true;
+        }
+        catch (System.IO.IOException ex)
+        {
+          throw ex;
+        }
+      }
+      return deleteSuccess;
+    }
+  }
+}
---
+++ src/Web.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+/// <summary>
+/// Wrapped Quick Web Utilities
+/// <summary>
+namespace Utility.Web
+{
+  interface IMail
+  {
+    void SetCredentials(string username, string password);
+
+    bool Validate();
+
+    bool SendMail();
+  }
+
+  public class GMail : IMail
+  {
+    private System.Net.NetworkCredential i_CurrentCredential;
+    private int i_DefaultPort;
+    private string i_DefaultHost;
+    private bool i_EnableSSL;
+    private string i_FromAddress;
+    private string i_ToAddress;
+    private string i_SubjectStr;
+    private string i_ContentStr;
+
+    public GMail() 
+    {
+      i_DefaultPort = 587;
+      i_DefaultHost = "smtp.gmail.com";
+      i_EnableSSL = true;
+      i_SubjectStr = "[Sample Subject]";
+      i_ContentStr = "[Sample Content]";
+    }
+
+    public GMail(string username, string password):this()
+    {
+      SetCredentials(username, password);
+    }
+
+    #region IMail Members
+
+    public void SetCredentials(string username, string password)
+    {
+      i_CurrentCredential = new System.Net.NetworkCredential(username, password);
+      if (username.Contains("@")) { i_FromAddress = username; }
+    }
+
+    public bool Validate()
+    {
+      if (i_CurrentCredential == null) { return false; }
+      if (string.IsNullOrEmpty(i_FromAddress) || string.IsNullOrEmpty(i_ToAddress)) { return false; }
+      return true;
+    }
+
+    public bool SendMail()
+    {
+      if (!Validate()) { return false; }
+      System.Net.Mail.SmtpClient mailClient = new System.Net.Mail.SmtpClient();
+      mailClient.Credentials = i_CurrentCredential;
+      mailClient.Port = i_DefaultPort;
+      mailClient.Host = i_DefaultHost;
+      mailClient.EnableSsl = i_EnableSSL;
+
+      System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+      message.From = new System.Net.Mail.MailAddress(i_FromAddress, i_FromAddress, System.Text.Encoding.UTF8);
+      message.To.Add(i_ToAddress);
+      message.Subject = i_SubjectStr;
+      message.Body = i_ContentStr;
+      message.DeliveryNotificationOptions = System.Net.Mail.DeliveryNotificationOptions.OnFailure;
+      try
+      {
+        mailClient.Send(message);
+      }
+      catch (System.Net.Mail.SmtpException ex)
+      {
+        throw ex;
+      }
+      return true;
+    }
+
+    #endregion
+
+    public System.Net.NetworkCredential CurrentCredential
+    {
+      get { return i_CurrentCredential; }
+    }
+
+    public string FromAddress
+    {
+      get { return i_FromAddress; }
+      set { i_FromAddress = value; }
+    }
+
+    public string ToAddress
+    {
+      get { return i_ToAddress; }
+      set { i_ToAddress = value; }
+    }
+
+    public string Subject
+    {
+      get { return i_SubjectStr; }
+      set { i_SubjectStr = value; }
+    }
+
+    public string Content
+    {
+      get { return i_ContentStr; }
+      set { i_ContentStr = value; }
+    }
+  }
+
+  interface IContent
+  {
+    void SetCredentials(string username, string password);
+
+    string GetContent(string url);
+  }
+
+  public class HttpContent : IContent
+  {
+    private System.Net.NetworkCredential i_CurrentCredential;
+
+    public HttpContent() { }
+
+    public HttpContent(string username, string password) : this()
+    {
+      SetCredentials(username, password);
+    }
+
+    #region IContent Members
+
+    public void SetCredentials(string username, string password)
+    {
+      i_CurrentCredential = new System.Net.NetworkCredential(username, password);
+    }
+
+    public string GetContent(string url)
+    {
+      string content = string.Empty;
+      System.Net.WebRequest webRequest = System.Net.WebRequest.Create(url);
+      System.Net.HttpWebRequest httpRequest = (System.Net.HttpWebRequest)webRequest;
+      httpRequest.Method = "GET";
+      try
+      {
+        System.Net.WebResponse webResponse = httpRequest.GetResponse();
+        System.IO.Stream receivedStream = webResponse.GetResponseStream();
+        using (System.IO.StreamReader reader = new System.IO.StreamReader(receivedStream, System.Text.Encoding.UTF8))
+        {
+          content = reader.ReadToEnd().Trim();
+          reader.Close();
+          receivedStream.Close();
+        }
+      }
+      catch(System.Net.WebException ex)
+      {
+        throw ex;
+      }
+      return content;
+    }
+
+    public static string GetQuickContent(string url)
+    {
+      HttpContent content = new HttpContent();
+      return content.GetContent(url);
+    }
+
+    #endregion
+
+    public System.Net.NetworkCredential CurrentCredential
+    {
+      get { return i_CurrentCredential; }
+    }
+  }
+}
